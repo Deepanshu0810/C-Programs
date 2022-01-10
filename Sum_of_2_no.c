62a3fa96b51b2cc5081c7c04e18a38e86cb6495a
Deepanshu0810 <deepanshu0810@gmail.com>
2022-01-10 19:59:35 +0530

    Initial Commit

---
+++ Sum_of_2_no.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter The value of b:");
+    scanf("%d",&b);
+    printf("The value of a+b is %d+%d=%d",a,b,a+b);
+    return 0;
+}
